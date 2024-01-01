1848a33161578f172b5ebbb96285d823f915dbe9
Nurdin Jumanne Athuman <nrdout@gmail.com>
2024-01-01 22:45:58 +0300

    few chnage

--- src/app/components/FormStep/AddOns/index.tsx
+++ src/app/components/FormStep/AddOns/index.tsx
@@ -12,8 +12,9 @@ import { AddonWithPrices, Addon } from "../../../types/add-ons";
 import ADD_ONS from '../../../../data/add-ons.json'
 import { FromToCard } from "./FromToCard";
 import Datepicker from "react-tailwindcss-datepicker";
-import {Tabs, Tab, Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import {Tabs, Tab, Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, ScrollShadow} from "@nextui-org/react";
 
+import TIMETABLE from '../../../../data/timetable.json'
 
 export type RadioAd = {
   id: string;
@@ -34,27 +35,98 @@ export type RadioTimetableTabs = {
   radios: [Radio];
   
 }
-
+function addDays(date: string, days: any) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
 
 function generateDummyData(selectedRadioDateRange: any){
+
+  let radioArray = [
+    {
+      id: "radio-1",
+      name: "Clouds FM",
+      logo: "https://scontent.fdar11-1.fna.fbcdn.net/v/t39.30808-1/339924489_1372068046706863_7473013869016800822_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=109&ccb=1-7&_nc_sid=4da83f&_nc_ohc=mqTI0vjMWTkAX9a7Tqu&_nc_ht=scontent.fdar11-1.fna&oh=00_AfAJ9ZyH5rs_c1BDR91MpOv3X6iqqw2qvkC4n_q3U6TRug&oe=65962AB0",
+    },
+    {
+      id: "radio-2",
+      name: "EFM Radio",
+      logo: "https://scontent.fdar11-1.fna.fbcdn.net/v/t39.30808-1/339924489_1372068046706863_7473013869016800822_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=109&ccb=1-7&_nc_sid=4da83f&_nc_ohc=mqTI0vjMWTkAX9a7Tqu&_nc_ht=scontent.fdar11-1.fna&oh=00_AfAJ9ZyH5rs_c1BDR91MpOv3X6iqqw2qvkC4n_q3U6TRug&oe=65962AB0",
+    },
+    {
+      id: "radio-3",
+      name: "Radio ONE",
+      logo: "https://scontent.fdar11-1.fna.fbcdn.net/v/t39.30808-1/339924489_1372068046706863_7473013869016800822_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=109&ccb=1-7&_nc_sid=4da83f&_nc_ohc=mqTI0vjMWTkAX9a7Tqu&_nc_ht=scontent.fdar11-1.fna&oh=00_AfAJ9ZyH5rs_c1BDR91MpOv3X6iqqw2qvkC4n_q3U6TRug&oe=65962AB0",
+    },
+    {
+      id: "radio-4",
+      name: "Choice FM",
+      logo: "https://scontent.fdar11-1.fna.fbcdn.net/v/t39.30808-1/339924489_1372068046706863_7473013869016800822_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=109&ccb=1-7&_nc_sid=4da83f&_nc_ohc=mqTI0vjMWTkAX9a7Tqu&_nc_ht=scontent.fdar11-1.fna&oh=00_AfAJ9ZyH5rs_c1BDR91MpOv3X6iqqw2qvkC4n_q3U6TRug&oe=65962AB0",
+    }
+  ]
 
   const startDate: string  = selectedRadioDateRange?.startDate;
   const endDate: string    = selectedRadioDateRange?.endDate;
 
-  var diff = Math.abs(new Date(startDate).getTime() - new Date(endDate).getTime());
-  var diffDays = Math.ceil(diff / (1000 * 3600 * 24)); 
+  const diff = Math.abs(new Date(startDate).getTime() - new Date(endDate).getTime());
+  const diffDays = Math.ceil(diff / (1000 * 3600 * 24)); 
   console.log("Diff in Days: " + diffDays);
+
+  
+ 
 
 
     let dummyTimeTable = [];
     for (let i = 1; i < diffDays; i++) {
-        let newName = {
-          id:i.toString(),
-          name:"Tony"
+        let dateId = new Date(addDays(startDate, i).toString()).toLocaleDateString('en-GB')
+
+        let radios: any[] = []
+        radioArray.forEach( (element) => {
+
+          let newRadio: any = {
+            id: element.id,
+            name: element.name,
+            logo: element.logo,
+          };
+
+              let radioAds = []
+              for (let iRadioAd = 1; iRadioAd < 24; iRadioAd++) {
+                let newRadioAd = {
+                  id: newRadio.id+'-'+iRadioAd,
+                  name: iRadioAd, // change later
+                  timeFrom: iRadioAd, // change later
+                  timeTo: iRadioAd, // change later
+                  price: '1000000',
+                };
+                radioAds.push(newRadioAd)
+              }
+
+              newRadio.radioAds = radioAds
+
+              radios.push(newRadio)
+          
+         
+      });
+
+        
+       
+
+        let newTimetable = {
+          id: dateId,
+          label: dateId,
+          radios: radios
         };
-        dummyTimeTable.push(newName);
+        dummyTimeTable.push(newTimetable);
     }
     console.log("dummyTimeTable==> ",dummyTimeTable)
+
+
+    
+
+    
+
+    return dummyTimeTable;
 
 }
 
@@ -68,28 +140,51 @@ export function AddOns() {
 	// });
 
   
-  const radioTimetableTabs: RadioTimetableTabs[] = [
-    {
-      id: "01/01/2024",
-      label: "01/01/2024",
-      radios: [
-        {
-          id: "clouds",
-          name: "Clouds FM",
-          logo: "https://scontent.fdar11-1.fna.fbcdn.net/v/t39.30808-1/339924489_1372068046706863_7473013869016800822_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=109&ccb=1-7&_nc_sid=4da83f&_nc_ohc=mqTI0vjMWTkAX9a7Tqu&_nc_ht=scontent.fdar11-1.fna&oh=00_AfAJ9ZyH5rs_c1BDR91MpOv3X6iqqw2qvkC4n_q3U6TRug&oe=65962AB0",
-          radioAds: [
-            {
-              id: "string",
-              name: "string",
-              timeFrom: "string",
-              timeTo: "string",
-              price: "string",
-            }
-          ]
-        }
-      ]
-    }
-  ];
+  // let radioTimetableTabs: RadioTimetableTabs[] = [
+  //   {
+  //     id: "01/01/2024",
+  //     label: "01/01/2024",
+  //     radios: [
+  //       {
+  //         id: "clouds",
+  //         name: "Clouds FM",
+  //         logo: "https://scontent.fdar11-1.fna.fbcdn.net/v/t39.30808-1/339924489_1372068046706863_7473013869016800822_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=109&ccb=1-7&_nc_sid=4da83f&_nc_ohc=mqTI0vjMWTkAX9a7Tqu&_nc_ht=scontent.fdar11-1.fna&oh=00_AfAJ9ZyH5rs_c1BDR91MpOv3X6iqqw2qvkC4n_q3U6TRug&oe=65962AB0",
+  //         radioAds: [
+  //           {
+  //             id: "string",
+  //             name: "string",
+  //             timeFrom: "string",
+  //             timeTo: "string",
+  //             price: "string",
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // ];
+
+  // let tabs = [
+  //   {
+  //     id: "photos",
+  //     label: "Photos",
+  //     content: "Lorem ipsum photos."
+  //   },
+  //   {
+  //     id: "music",
+  //     label: "Music",
+  //     content: "Lorem ipsum music."
+  //   },
+  //   {
+  //     id: "videos",
+  //     label: "Videos",
+  //     content: "Lorem ipsum videos."
+  //   }
+  // ];
+
+    
+  let radioTimetableTabs: any[] = [];
+  const [radioTimetableData, setRadioTimetableData] = useState([])
+  const [tabData, setTabData] = useState([])
 
 
   const { addOns, setAddOns, selectedRadioDateRange, setSelectedRadioDateRange, isCoupon } = useForm()
@@ -124,9 +219,40 @@ export function AddOns() {
     setSelectedRadioDateRange(newValue)
     saveValueToLocalStorage('radio-date-range', JSON.stringify(newValue))
 
-    generateDummyData(newValue)
+    // radioTimetableTabs = generateDummyData(newValue)
+
+    let resultValue: any = generateDummyData(newValue)
+
+    setRadioTimetableData(resultValue)
+
+
+    let tabs: any = [
+    {
+      id: "photos",
+      label: "Photos",
+      content: "Lorem ipsum photos."
+    },
+    {
+      id: "music",
+      label: "Music",
+      content: "Lorem ipsum music."
+    },
+    {
+      id: "videos",
+      label: "Videos",
+      content: "Lorem ipsum videos."
+    }
+  ];
+  var JsonObject = JSON.parse(JSON.stringify(tabs));
+
+  setTabData(JsonObject)
 
 	};
+
+  // const onSelectionChange = (selectedKey: any) => {
+	// 	console.log('selectedKey newValue:', selectedKey);
+
+  // }
 
 
   return (
@@ -157,6 +283,8 @@ export function AddOns() {
               />
           </div>
 
+     
+
           {selectedRadioDateRange  &&
              <div className="flex w-full flex-col">
               
@@ -167,63 +295,31 @@ export function AddOns() {
                </CardBody>
              </Card> */}
 
-             <Tabs aria-label="Radio timetable" items={radioTimetableTabs}>
-              {(item) => (
-                <Tab key={item.id} title={item.label}>
-                  
-                      {/* {item.content} */}
 
-
-                      {item.radios.map((radioItem, radioIndex) => (
+              {/* {radioTimetableData.map((radioTimetableTab: any, radioIndex: any) => (
                           <>
-                              <Card>
-                                <CardHeader className="flex gap-3">
-                                  <Image
-                                    alt={radioItem?.name}
-                                    height={40}
-                                    radius="sm"
-                                    src={radioItem.logo}
-                                    width={40}
-                                  />
-                                  <div className="flex flex-col">
-                                    <p className="text-md">{radioItem?.name}</p>
-                                    {/* <p className="text-small text-default-500">{radioItem?.name}</p> */}
-                                  </div>
-                                </CardHeader>
-                                <Divider/>
-                                <CardBody>
-                                  {ADD_ONS.map((addOn, index) => (
-                                        <>
-                                        <AddOnOption
-                                          key={index}
-                                          addOn={addOn}
-                                          isSelected={!!addOns.find(({ title }) => addOn.title === title)}
-                                          handleSelectAddon={handleSelectAddon}
-                                          handleUnselectedAddon={handleUnselectedAddon}
-                                        />
-                                        <p></p>
-                                        </>
-                                      ))}
-                                </CardBody>
-                                <Divider/>
-                                {/* <CardFooter>
-                                  <Link
-                                    isExternal
-                                    showAnchorIcon
-                                    href="https://github.com/nextui-org/nextui"
-                                  >
-                                    Visit source code on GitHub.
-                                  </Link>
-                                </CardFooter> */}
-                                </Card>
+                          <Tabs aria-label="Radio timetable" items={radioTimetableTab}></Tabs>
+
+                          
                           </>
-                        ))}
-
-                     
-
-                </Tab>
-              )}
-            </Tabs>
+                           ))} */}
+ <ScrollShadow orientation="horizontal" className="max-w-[500px] max-h-[500px]">
+      {/* <Content className="w-[800px]" /> */}
+   
+<div className="flex flex-wrap gap-4">
+        <Tabs aria-label="Timetables" items={TIMETABLE} fullWidth={false} color="warning" variant='bordered'>
+        {(item) => (
+          <Tab key={item.id} title={item.label}>
+            <Card>
+              <CardBody>
+                {item.id}
+              </CardBody>
+            </Card>  
+          </Tab>
+        )}
+      </Tabs>
+    </div>  
+    </ScrollShadow>
 
              </div>  
 
